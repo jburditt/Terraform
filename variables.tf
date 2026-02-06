1843ff589a32dae7585b9cf5a064cf71d3e26fbe
Jebb Burditt <jebb.burditt@gmail.com>
2026-02-05 16:23:22 -0800

    Add Terraform variables for resource configuration

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  default = "rg-gamifyworkout"
+}
+
+variable "static_web_app_name" {
+  default = "stapp-gamifyworkout"
+}
+
+variable "location" {
+  default = "westus2"
+}
+
+variable "domain" {
+  default = "gamifyworkout.com"
+}
+
+variable "subdomain" {
+  default = "www"
+}
